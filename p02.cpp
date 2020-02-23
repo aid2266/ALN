6aa7120a93907de21e461ad35d500a51360f1840
Aidan Deaves <33912342+aid2266@users.noreply.github.com>
2020-02-24 00:22:25 +0100

    I have modified p02

--- p02.cpp
+++ p02.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
 
 int main(){
     
-    // hello this is a test
+    float sumUp = 0;
+    float sumDown = 0;
+    for (float i = 1; i <= 15; i++) sumUp += 1 / (i * i);
+    cout << "Sum Up: " << setprecision(4) << sumUp << endl;
+    // como escoger por eliminacion o sin eliminacion?
     
+    for (float i = 15; i >= 1; i--) sumDown += 1 / (i * i);
+    cout << "Sum Down: " << setprecision(4) << sumDown << endl;
 
 }
