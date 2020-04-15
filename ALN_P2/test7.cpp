81743e1bd7c15acbd6a35aa2238c7e1fa5ce3e1e
Aidan Deaves <33912342+aid2266@users.noreply.github.com>
2020-04-15 14:23:25 +0200

    lu decomposition working at 100%, the error is correct and determinant as well

--- ALN_P2/test7.cpp
+++ ALN_P2/test7.cpp
@@ -73,6 +73,12 @@ int main(){
 
 int LUPDecompose(MD& A, VD& b,  int N, double Tol) {
 
+    /* DISCLAIMER:
+     este algoritmo para descomponer A en LU se realiza sobre la matriz A.
+     La matriz descompuesta A' = L + U. Donde L es una mat-tri-inf con 0's
+     en la diagonal (añadimos 1's a posteriori) y U una mat-tri-sup.
+    */
+    
     int imax;
     int perm_counter = 0;
     MD L(N, VD(N));     // triangular inferior
@@ -81,18 +87,18 @@ int LUPDecompose(MD& A, VD& b,  int N, double Tol) {
     MD A_Copy(N, VD(N));   // copia de la matriz
     A_Copy = A;
     
-    // vector de perm dim N
-    for (int i = 0; i < N; i++) P[i] = i; //Unit permutation matrix, P[N] initialized with N
+    for (int i = 0; i < N; i++) P[i] = i; //init vect permutacion unitario
 
+    // ** FACTORIZACION LU guardado en la matriz A** //
     for (int i = 0; i < N; i++) {
         int imax = find_max_pivot(A, N, i); // pos pivote max, parcial escalonado
-        if (abs(A[imax][i]) < tol) return 0; //failure, matrix is degenerate
+        if (abs(A[imax][i]) < tol) return 0; //matriz es degenerada
         swap(A[i], A[imax]);
         swap(P[i], P[imax]);
-        perm_counter++;
+        perm_counter++; // contador del numero de permutaciones
 
         for (int j = i + 1; j < N; j++) {
-            A[j][i] /= A[i][i]; // Triangular inferior
+            A[j][i] /= A[i][i]; // calculo del multiplicador
 
             for (int k = i + 1; k < N; k++){
                 A[j][k] -= A[j][i] * A[i][k];
@@ -100,7 +106,7 @@ int LUPDecompose(MD& A, VD& b,  int N, double Tol) {
         }
     }
     
-    // encontramos L y U dentro de A
+    // Hallamos L y U dentro de A descompuesta, recordamos A' = L + U
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
             if (i == j) {
@@ -112,24 +118,30 @@ int LUPDecompose(MD& A, VD& b,  int N, double Tol) {
         }
     }
     
-    // ** Calculo del determinante ** //
+    // ** CALCULO DEL DETERMINANTE ** //
     double det = 1.;
     for (int i = 0; i < N; i++) det *= U[i][i];
     
-    /*
-    cout << "this is A, contains L and U" << endl;
-    write(A, N);
-    cout << endl;
-    cout << "this is U" << endl;
-    write(U, N);
-    cout << "this is L" << endl;
-    write(L, N);
-    cout << "determinante: " << det << endl;
-    */
-    
     // ** CALCULO DEL ERROR |PA - LU| norma 1 ** //
+    MD LU(N, VD(N)); // matriz producto L*U
     
-    
+    double norma_1 = 0.;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++){
+                LU[i][j] += L[i][k] * U[k][j]; // calculamos LU
+            }
+            norma_1 = abs(A_Copy[P[i]][j] - LU[i][j]); // PA - LU
+        }
+    }
+    cout << "Matriz A descompuesta en LU" << endl;
+    write(LU, N);
+    cout << "Vector permutacion: " << '[';
+    writeV(P, N);
+    cout << ']' << endl;
+    cout << "dim A: " << N << endl;
+    cout << "determinante: " << det << endl;
+    cout << "error |PA - LU|_1: " << norma_1 << endl;
     
     LUPSolve(A, P, b, N);
     
@@ -137,20 +149,15 @@ int LUPDecompose(MD& A, VD& b,  int N, double Tol) {
     else return -1;              // numero impar de ops
 }
 
-
-
-
-
 /* INPUT: A,P filled in LUPDecompose; b - rhs vector; N - dimension
  * OUTPUT: x - solution vector of A*x=b
  */
 void LUPSolve(MD& A, VD& P, VD& b, int N) {
-
-    VD x(N);
+    
+    VD x(N); // creamos el vector solucion
     
     for (int i = 0; i < N; i++) {
-        x[i] = b[P[i]];
-
+        x[i] = b[P[i]]; // solucion permutada
         for (int k = 0; k < i; k++)
             x[i] -= A[i][k] * x[k];
     }
@@ -211,7 +218,7 @@ double LUPDeterminant(double **A, int *P, int N) {
     cout << "determinante del sistema " << det << endl;
 }
 
-
+// escribe matriz
 void write(MD& A, int n){
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
@@ -220,6 +227,16 @@ void write(MD& A, int n){
         cout << endl;
     }
     cout << endl; cout << endl; cout << endl;
+}
+
+// escribe vector
+void writeV(VD& b, int n){
+    cout << '[';
+    for (int j = 0; j < n; j++){
+            cout << b[j] << ' ';
+        }
+    cout << ']' << endl;
+    
 }
 
 int find_max_pivot (const MD& A, int n, int k){
